2c2d89c82c308eb6de86be52fae6254d2be94a34
Amirul898 <amirul898amir@gmail.com>
2023-10-09 00:17:39 +0600

    read: floating button appbar buttonnavigationbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class AmrApp extends StatelessWidget{
   Widget build(BuildContext context) {
 
     return  MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.pink),
+      theme: ThemeData(primarySwatch: Colors.green),
       darkTheme: ThemeData(primarySwatch: Colors.cyan),
       debugShowCheckedModeBanner: false,
       home:  const HomeActivaty(),
@@ -20,6 +20,16 @@ class AmrApp extends StatelessWidget{
   }
 }
 
+
+//Snakmessage
+thisSnakBar(msg,context ){
+  return ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(content: Text(msg))
+  );
+}
+
+
+//HomeActivaty
 class HomeActivaty extends StatelessWidget{
   const HomeActivaty({super.key});
 
@@ -28,9 +38,8 @@ class HomeActivaty extends StatelessWidget{
 
     return Scaffold(
 
-
       appBar:AppBar(
-        title: Text("AmrApp"),
+        title: const Text("AmrApp"),
         titleSpacing: 20 ,
         centerTitle: false,
         toolbarHeight:40 ,
@@ -38,16 +47,47 @@ class HomeActivaty extends StatelessWidget{
         elevation: 20,
         backgroundColor: Colors.indigo,
         actions: [
-          IconButton(onPressed: (){}, icon: Icon(Icons.ice_skating))
+          IconButton(onPressed: (){thisSnakBar ("I Am appbar", context);}, icon: const Icon(Icons.ice_skating)),
+          IconButton(onPressed: (){thisSnakBar ("I Am appbar", context);}, icon: const Icon(Icons.ice_skating)),
+          IconButton(onPressed: (){thisSnakBar ("I Am appbar", context);}, icon: const Icon(Icons.ice_skating))
         ],
-
       ),
 
+      floatingActionButton: FloatingActionButton(
+        elevation: 10,
+        onPressed: () { thisSnakBar("This is floating action button",context ); },
+        // backgroundColor: Color(Colors.pink),
+        child: const Icon(Icons.add_circle),
+      ),
 
-      body: Text("this is body.................."),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex:0,
+        items: [
+          BottomNavigationBarItem(icon:Icon (Icons.home), label: "home"),
+          BottomNavigationBarItem(icon:Icon (Icons.account_circle_outlined), label: "home"),
+          BottomNavigationBarItem(icon:Icon (Icons.mail_sharp), label: "home")
+        ],
+
+
+
+        onTap: (int index){
+          if (index==0){
+            thisSnakBar("I am footer home ", context);
+          }
+          if (index==1){
+            thisSnakBar("I am footer profile ", context);
+          }
+          if (index==2){
+            thisSnakBar("I am footer contact ", context);
+          }
+        },
+
+        elevation: 10,
+        backgroundColor: Colors.grey,
+        iconSize: 25,
+      ),
 
     );
-
   }
 
 
